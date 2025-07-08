01d970e508dda8f1e944afd6a26925821351d16a
KernelZhiShui <1440248234.com>
2025-07-08 15:36:32 +0800

    fix(user): UserDao file location

--- src/main/java/org/example/springmvc/dao/impl/UserDaoImpl.java
+++ src/main/java/org/example/springmvc/dao/impl/UserDaoImpl.java
@@ -1,6 +1,7 @@
-package org.example.springmvc.dao;
+package org.example.springmvc.dao.impl;
 
 import org.example.springmvc.bean.User;
+import org.example.springmvc.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
